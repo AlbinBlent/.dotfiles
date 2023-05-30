cecfab0e4a22bf7feb8068c05dcef24dd2596fad
Albin Blent <albin.blent@gmail.com>
2023-05-30 13:42:14 +0200

    updated install.sh

--- install.sh
+++ install.sh
@@ -7,8 +7,19 @@
 #install from Brewfile
 brew bundle
 
+# Install AstroNvim
+git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+
 # Install tpm - tmux plugin manager
 /bin/bash -c "git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm"
+
+# source tmux file (tmux source .dotfiles/.config/tmux/tmux.config) -> install plugins: in tmux run prefix + I
+
+# install zsh-autosuggestions plugin
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# install zsh-syntax-highlighting plugin
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # Symlink configs
 /bin/bash -c "./symlink.sh"
